dfb4032ca7598d0dd98b2edc7c37a6af1bab5715
abc <nsected@gmail.com>
2025-06-30 21:26:47 +0500

    Add text message handler to reply on user messages

--- api/webhook.js
+++ api/webhook.js
@@ -2,10 +2,10 @@ import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Пример простой команды
-bot.command('start', (ctx) => ctx.reply('Привет! Бот запущен.'));
+bot.command('start', (ctx) => ctx.reply('Привет! Бот telegram-vercel-openai-bot2843 запущен.'));
 
-// Логирование для проверки работы
+bot.on('text', (ctx) => ctx.reply('Привет! Я получил твоё сообщение.'));
+
 console.log('Bot initialized');
 
 export default async function handler(req, res) {
